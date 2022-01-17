9d2000dfb2f8a39a00980d746183fe460a6c827a
Arne Meeuw <arne.meeuw@gmail.com>
2022-01-17 21:27:48 +0100

    Time for a tiny update

---
+++ tilesets/Gather Chairs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Gather Chairs" tilewidth="32" tileheight="32" tilecount="800" columns="25">
+ <image source="../../tilemap_mountain_hut/mapmaking/tilesets/gather_chairs_1.3.png" width="800" height="1024"/>
+</tileset>
