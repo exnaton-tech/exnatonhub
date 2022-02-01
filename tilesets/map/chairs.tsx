d8a943185d77a39a0cc67d415dc9ecf4a4268012
Arne Meeuw <arne.meeuw@gmail.com>
2022-02-01 20:24:57 +0100

    Reimported tilesets

---
+++ tilesets/map/chairs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Chairs" tilewidth="32" tileheight="32" tilecount="800" columns="25">
+ <image source="../mapmaking/gather_chairs_1.3.png" width="800" height="1024"/>
+</tileset>
---
+++ tilesets/map/floors.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Floors" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../tileset_floor.png" width="256" height="256"/>
+ <tile id="37">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
